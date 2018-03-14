3fbf9c26092febb4a988e52f013a3c002bd00f4a
Mateusz <podolak.3@gmail.com>
2018-03-14 06:59:08 +0100

    Poprawiono Waity

--- src/main/java/com/testuj/selenium/configuration/Waits.java
+++ src/main/java/com/testuj/selenium/configuration/Waits.java
@@ -1,11 +1,16 @@
 package com.testuj.selenium.configuration;
 
+import com.google.common.base.Function;
 import org.awaitility.Awaitility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -21,7 +26,20 @@ public class Waits {
         webDriverWait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    public static void fluentWaitForVisiblityOfElement(WebElement element) {
+    public static WebElement fluentWaitForVisibilityOfElement(String selector) {
+        Wait<WebDriver> wait = new FluentWait<>(Driver.getInstance())
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        return wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector(selector));
+            }
+        });
+    }
+
+    public static void awaitilityWaitForVisiblityOfElement(WebElement element) {
         Awaitility.with().await()
                 .pollInterval(1, TimeUnit.SECONDS)
                 .atMost(15, TimeUnit.SECONDS)
